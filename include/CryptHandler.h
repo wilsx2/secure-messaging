080139309250c51cd273019966f6a1a5afc240d9
William Conner <redwils5@gmail.com>
2026-01-19 18:34:33 -0500

    Declared CryptHandler

--- include/CryptHandler.h
+++ include/CryptHandler.h
@@ -4,17 +4,24 @@
 #include <cryptopp/modes.h>
 #include <cryptopp/aes.h>
 #include <cryptopp/filters.h>
+#include <array>
 
-// TODO: Generics for
+// TODO: Generics for keylength
 class CryptHandler
 {
     private:
-    CryptoPP::byte key[CryptoPP::AES::DEFAULT_KEYLENGTH];
-    CryptoPP::AES::Encryption aesEncryption;
-    CryptoPP::AES::Decryption aesDecryption;
+    static constexpr std::size_t KEYLENGTH = CryptoPP::AES::DEFAULT_KEYLENGTH;
+    std::array<CryptoPP::byte, KEYLENGTH> _key;
+    CryptoPP::AES::Encryption _aesEncryption;
+    CryptoPP::AES::Decryption _aesDecryption;
 
     public:
-    CryptHandler(std::string key)
+    CryptHandler(std::array<CryptoPP::byte, KEYLENGTH> key);
+
+    std::string Encrypt(const std::string& plaintext);
+    std::string Decrypt(const std::string& ciphertext);
+
+    static std::array<CryptoPP::byte, KEYLENGTH> KeyFromString(const std::string& string); // TODO: Use PBKDF2
 }
 
 #endif
