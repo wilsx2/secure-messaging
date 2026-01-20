ff5b06790736785b483c94e1a965081067b068a5
William Conner <redwils5@gmail.com>
2026-01-20 18:08:54 -0500

    Implemented AsymCryptHandler::Encrypt() and made rng of the constructor a static member

--- include/AsymCryptHandler.h
+++ include/AsymCryptHandler.h
@@ -2,10 +2,12 @@
 #define A_CRYPT_H
 
 #include <cryptopp/rsa.h>
+#include <cryptopp/osrng.h>
 
 class AsymCryptHandler
 {
     private:
+    static CryptoPP::AutoSeededRandomPool _rng;
     CryptoPP::RSA::PrivateKey _rsa_private_key;
     CryptoPP::RSA::PublicKey _rsa_public_key;
 
@@ -13,12 +15,12 @@ class AsymCryptHandler
     AsymCryptHandler();
     AsymCryptHandler(std::size_t key_size);
 
+    static std::string Encrypt(const std::string& plaintext, const CryptoPP::RSA::PublicKey& key);
     std::string Decrypt(const std::string& ciphertext);
 
     const CryptoPP::RSA::PublicKey& GetPublicKey();
     const CryptoPP::RSA::PrivateKey& GetPrivateKey();
 
-    static std::string Encrypt(const std::string& plaintext, const CryptoPP::RSA::PublicKey& key);
     // static std::string KeyToString(const std::array<CryptoPP::byte, KEYLENGTH>& key);
 };
 
--- src/AsymCryptHandler.cpp
+++ src/AsymCryptHandler.cpp
@@ -1,14 +1,28 @@
 #include "AsymCryptHandler.h"
 
-#include <cryptopp/osrng.h>
-
 using namespace CryptoPP;
+
+AutoSeededRandomPool AsymCryptHandler::_rng;
 
 AsymCryptHandler::AsymCryptHandler(): AsymCryptHandler(2048) {};
 
 AsymCryptHandler::AsymCryptHandler(std::size_t key_size)
 {
-    static AutoSeededRandomPool rng;
-    _rsa_private_key.GenerateRandomWithKeySize(rng, key_size);
+    _rsa_private_key.GenerateRandomWithKeySize(_rng, key_size);
     _rsa_public_key = RSA::PublicKey(_rsa_private_key);
+}
+
+std::string AsymCryptHandler::Encrypt(const std::string& plaintext, const RSA::PublicKey& key)
+{
+    std::string ciphertext;
+
+    RSAES_OAEP_SHA_Encryptor e(key);
+
+    StringSource ss1(plaintext, true,
+        new PK_EncryptorFilter(_rng, e,
+            new StringSink(ciphertext)
+        )
+    );
+
+    return ciphertext;
 }
