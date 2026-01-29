faa2611f13bc8815be7e1b08fa0eee33e798cd2d
William Conner <redwils5@gmail.com>
2026-01-29 17:20:29 -0500

    Created test for sockets

--- tests/sockets.cpp
+++ tests/sockets.cpp
@@ -1,9 +1,46 @@
-#include <iostream>
 #include "TcpSocket.h"
+#include <thread>
+#include <iostream>
+#include <unistd.h>
 
-int main(int argc, char* argv[])
+constexpr int PORT = 5555;
+
+void server()
 {
-    TcpSocket server (2000, INADDR_ANY);
-    TcpSocket client (2001, INADDR_ANY);
+    TcpSocket serverSocket(PORT, INADDR_ANY);
 
+    serverSocket.Bind();
+    serverSocket.Listen(1);
+
+    TcpSocket client = serverSocket.Accept();
+
+    std::string msg = client.Read(1024);
+    std::cout << "[Server] Received: " << msg << std::endl;
+
+    client.Write("Hello from server");
+}
+
+void client()
+{
+    // Give server time to start
+    sleep(1);
+
+    TcpSocket clientSocket(PORT, INADDR_LOOPBACK);
+    clientSocket.Connect();
+
+    clientSocket.Write("Hello from client");
+
+    std::string response = clientSocket.Read(1024);
+    std::cout << "[Client] Received: " << response << std::endl;
+}
+
+int main()
+{
+    std::thread serverThread(server);
+    std::thread clientThread(client);
+
+    serverThread.join();
+    clientThread.join();
+
+    return 0;
 }
