3bb1f123b12797a8ac986abf4b0ff21da3875bbc
William Conner <redwils5@gmail.com>
2026-01-29 20:01:11 -0500

    Added secure channel to secure_sockets test

--- tests/secure_sockets.cpp
+++ tests/secure_sockets.cpp
@@ -1,3 +1,4 @@
+#include "SecureChannel.h"
 #include "TcpSocket.h"
 #include <thread>
 #include <iostream>
@@ -16,13 +17,6 @@ constexpr int PORT = 5555;
 
 void server()
 {
-    OID CURVE = ASN1::secp256r1();
-    AutoSeededRandomPool rng;
-    ECDH < ECP >::Domain dh( CURVE );
-    SecByteBlock priv(dh.PrivateKeyLength()), pub(dh.PublicKeyLength()), pubClient(dh.PublicKeyLength());
-    dh.GenerateKeyPair(rng, priv, pub);
-    SecByteBlock shared(dh.AgreedValueLength());
-
     TcpSocket serverSocket(PORT, INADDR_ANY);
 
     serverSocket.Bind();
@@ -30,17 +24,7 @@ void server()
 
     TcpSocket client = serverSocket.Accept();
 
-    client.Read(pubClient.BytePtr(), pubClient.SizeInBytes()); // Receive public key
-    client.Write(pub.BytePtr(), pub.SizeInBytes()); // Send public key
-
-    if(!dh.Agree(shared, priv, pubClient))
-    {
-        cout << "[Server] Failed to reach shared secret" << endl;
-        return;
-    }
-    Integer ss;
-    ss.Decode(shared.BytePtr(), shared.SizeInBytes());
-    cout << "[Server] Shared secret: " << hex << ss << endl;
+    SecureChannel channel (client, HostType::Server);
 
     client.Close();
     serverSocket.Close();
@@ -48,31 +32,14 @@ void server()
 
 void client()
 {
-    OID CURVE = ASN1::secp256r1();
-    AutoSeededRandomPool rng;
-    ECDH < ECP >::Domain dh( CURVE );
-    SecByteBlock priv(dh.PrivateKeyLength()), pub(dh.PublicKeyLength()), pubServer(dh.PublicKeyLength());
-    dh.GenerateKeyPair(rng, priv, pub);
-    SecByteBlock shared(dh.AgreedValueLength());
-
     // Give server time to start
     sleep(1);
 
     TcpSocket clientSocket(PORT, INADDR_LOOPBACK);
     clientSocket.Connect();
 
-    clientSocket.Write(pub.BytePtr(), pub.SizeInBytes()); // Send public key
-    clientSocket.Read(pubServer.BytePtr(), pubServer.SizeInBytes()); // Receive public key
-    
-    if(!dh.Agree(shared, priv, pubServer))
-    {
-        cout << "[Client] Failed to reach shared secret" << endl;
-        return;
-    }
+    SecureChannel channel (clientSocket, HostType::Client);
 
-    Integer ss;
-    ss.Decode(shared.BytePtr(), shared.SizeInBytes());
-    cout << "[Client] Shared secret: " << hex << ss << endl;
 
     clientSocket.Close();
 }
