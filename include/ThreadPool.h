87461dec9b7cd878d4e517dffa57caa04ff51e9e
William Conner <redwils5@gmail.com>
2026-01-29 21:45:02 -0500

    Declared and implemented ThreadPool

---
+++ include/ThreadPool.h
@@ -0,0 +1,30 @@
+#ifndef THREAD_POOL_H
+#define THREAD_POOL_H
+
+#include <vector>
+#include <thread>
+#include <queue>
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+
+class ThreadPool
+{
+    private:
+    std::queue<std::function<void()>> _job_queue;
+    std::mutex _queue_mutex;
+    std::condition_variable _mutex_condition;
+    std::vector<std::thread> _threads;
+    std::size_t _max_threads;
+    bool _deconstructing;
+
+    void ThreadLoop();
+    
+    public:
+    ThreadPool();
+    ~ThreadPool();
+    void Enqueue(std::function<void()> job);
+    bool Busy();
+};
+
+#endif
---
+++ src/ThreadPool.cpp
@@ -0,0 +1,64 @@
+// Credit: https://stackoverflow.com/questions/15752659/thread-pooling-in-c11
+#include "ThreadPool.h"
+#include <iostream>
+
+ThreadPool::ThreadPool()
+{
+    _deconstructing = false;
+    const int thread_count = std::thread::hardware_concurrency();
+    for(int i = 0; i < thread_count; ++i){
+        _threads.emplace_back(std::thread(&ThreadPool::ThreadLoop,this));
+    }
+}
+
+void ThreadPool::ThreadLoop()
+{
+    while(true)
+    {
+        std::function<void()> job;
+        {
+            std::unique_lock<std::mutex> lock (_queue_mutex);
+            _mutex_condition.wait(lock, [this] {
+                return !_job_queue.empty() || _deconstructing;
+            });
+            if(_deconstructing){
+                return;
+            }
+            job = _job_queue.front();
+            _job_queue.pop();
+        }
+        job();
+    }
+}
+
+void ThreadPool::Enqueue(std::function<void()> job)
+{
+    {
+        std::unique_lock<std::mutex> lock (_queue_mutex);
+        _job_queue.push(job);
+    }
+    _mutex_condition.notify_one();
+}
+
+bool ThreadPool::Busy()
+{
+    bool output;
+    {
+        std::unique_lock<std::mutex> lock (_queue_mutex);
+        output = !_job_queue.empty();
+    }
+    return output;
+}
+
+ThreadPool::~ThreadPool()
+{
+    {
+        std::unique_lock<std::mutex> lock (_queue_mutex);
+        _deconstructing = true;
+    }
+    _mutex_condition.notify_all();
+    for(std::thread& thread : _threads){
+        thread.join();
+    }
+    _threads.clear();
+}
